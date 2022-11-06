5186af216467b6c5fb01bd520a2cca9cdbf4cb40
Aaron-Starley-GCU <117592786+Aaron-Starley-GCU@users.noreply.github.com>
2022-11-06 11:26:08 -0700

    Activity1

---
+++ Activity1-AaronStarley/Activity1-AaronStarley/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Activity1_AaronStarley
+{
+    partial class Activity1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConfirmDateCheck = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ConfirmDateCheck
+            // 
+            this.ConfirmDateCheck.AutoSize = true;
+            this.ConfirmDateCheck.Location = new System.Drawing.Point(65, 101);
+            this.ConfirmDateCheck.Name = "ConfirmDateCheck";
+            this.ConfirmDateCheck.Size = new System.Drawing.Size(225, 19);
+            this.ConfirmDateCheck.TabIndex = 1;
+            this.ConfirmDateCheck.Text = "I verify that the date entered is correct";
+            this.ConfirmDateCheck.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(78, 53);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker.TabIndex = 3;
+            this.dateTimePicker.Value = new System.DateTime(1979, 7, 2, 11, 7, 0, 0);
+            this.dateTimePicker.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Location = new System.Drawing.Point(153, 9);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(56, 15);
+            this.TitleLabel.TabIndex = 4;
+            this.TitleLabel.Text = "Activity 1";
+            this.TitleLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(65, 149);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Location = new System.Drawing.Point(215, 149);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(75, 23);
+            this.confirmButton.TabIndex = 6;
+            this.confirmButton.Text = "Confirm";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            // 
+            // Activity1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 208);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.ConfirmDateCheck);
+            this.Name = "Activity1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private CheckBox ConfirmDateCheck;
+        private DateTimePicker dateTimePicker;
+        private Label TitleLabel;
+        private Button cancelButton;
+        private Button confirmButton;
+    }
+}
---
+++ Activity1-AaronStarley/Activity1-AaronStarley/Form1.cs
@@ -0,0 +1,33 @@
+/* This is my own original work. Activity 1 asks us build a simple user interface with a variety of
+controls. These controls don't do anything, but it isn't stated that they need to.
+ */
+namespace Activity1_AaronStarley
+{
+    public partial class Activity1 : Form
+    {
+        public Activity1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
