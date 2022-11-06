9ebb7d717e37ac7519ff538e0ec61ebdc527d40a
Aaron-Starley-GCU <117592786+Aaron-Starley-GCU@users.noreply.github.com>
2022-11-06 14:01:41 -0700

    Add files via upload

---
+++ Activity2-AaronStarley/Activity2-AaronStarley/Form1.cs
@@ -0,0 +1,38 @@
+/* Aaron Starley
+ * 11-6-22
+ * CST 150-O500
+ * 
+ * All of the work below is my own, besides the formula needed to convert from meters to Megahertz.
+ * I wanted to make sure my program was giving exact answers, so I looked up the exact conversion.
+ */
+namespace Activity2_AaronStarley
+{
+    public partial class Activity2 : Form
+    {
+
+        public Activity2()
+        {
+            InitializeComponent();
+        }
+
+        private void Activity2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            /* It is common to use 300 as an approximate conversion.
+             * But since I'm not doing the math in my head, and I need to cite the formula, 
+             * I may as well have my output be exact.
+             * 
+             * Citation:
+             * How to Convert Wavelength In Metres to Megahertz. 
+             * Convert wavelength in metres to megahertz. (n.d.). Retrieved November 6, 2022,
+             * from https://www.unitconverters.net/frequency-wavelength/wavelength-in-metres-to-megahertz.htm 
+             */
+            Double output = 299.792458 / Double.Parse(inputBox.Text);
+            outputBox.Text = System.Convert.ToString(output);
+        }
+    }
+}
