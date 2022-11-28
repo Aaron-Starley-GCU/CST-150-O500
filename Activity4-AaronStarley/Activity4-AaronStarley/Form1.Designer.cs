b7e8c265278b59fb3ae3db7dc93bd5c7ddb353a4
Aaron Starley <AStarley@my.gcu.edu>
2022-11-27 17:52:34 -0700

    Act4 Complete

---
+++ Activity4-AaronStarley/Activity4-AaronStarley/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Activity4_AaronStarley
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(21, 48);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 23);
+            this.inputBox.TabIndex = 1;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(21, 139);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(354, 23);
+            this.outputBox.TabIndex = 2;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(21, 96);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 3;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Location = new System.Drawing.Point(127, 56);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(50, 15);
+            this.inputLabel.TabIndex = 4;
+            this.inputLabel.Text = "seconds";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter number of seconds";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 205);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Time Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox inputBox;
+        private TextBox outputBox;
+        private Button convertButton;
+        private Label inputLabel;
+        private Label label1;
+    }
+}
---
+++ Activity4-AaronStarley/Activity4-AaronStarley/Form1.cs
@@ -0,0 +1,94 @@
+/*
+ Aaron Starley
+CST-150-O500
+
+Syntax that was new to this assignment (TryParse) was learned in the text:
+Gaddis, T. (2020). Starting out with Visual C#, 5/e. Pearson Education, Inc.
+
+Besides that, everything below (that wasn't automatically inserted by Visual Studio)
+is my own work.
+ */
+
+namespace Activity4_AaronStarley
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            outputBox.Text = "";
+            int minutes;
+            int hours;
+            int days;
+            int userIn;
+            if (!(int.TryParse(inputBox.Text, out userIn))||userIn<=0) {
+                outputBox.Text = System.Convert.ToString("Please enter a positive integer");
+            } else {
+                if (userIn >= 86400) { //Finds number of days and gives formatted output
+                    days = userIn / 86400;
+                    outputBox.Text += System.Convert.ToString(days);
+                    if (days > 1){
+                        outputBox.Text += System.Convert.ToString(" Days");
+                    } else {
+                        outputBox.Text += System.Convert.ToString(" Day");
+                    }
+                    userIn -= 86400 * days; //subtracts days from user input
+                    if (userIn > 0) {
+                        outputBox.Text += System.Convert.ToString(", ");
+                    }else{
+                        outputBox.Text += System.Convert.ToString(" ");
+                    }
+                }
+                if (userIn >= 3600){ //Finds number of hours and gives formatted output
+                    hours = userIn / 3600;
+                    outputBox.Text += System.Convert.ToString(hours);
+                    if (hours > 1){
+                        outputBox.Text += System.Convert.ToString(" Hours");
+                    } else {
+                        outputBox.Text += System.Convert.ToString(" Hour");
+                    }
+                    userIn -= 3600 * hours;//subtracts hours from user input
+                    if (userIn > 0){
+                        outputBox.Text += System.Convert.ToString(", ");
+                    } else {
+                        outputBox.Text += System.Convert.ToString(" ");
+                    }
+                }
+                if (userIn >= 60){ //Finds number of minutes and gives formatted output
+                    minutes = userIn / 60;
+                    outputBox.Text += System.Convert.ToString(minutes);
+                    if (minutes > 1){
+                        outputBox.Text += System.Convert.ToString(" Minutes");
+                    } else {
+                        outputBox.Text += System.Convert.ToString(" Minute");
+                    }
+                    userIn -= 60 * minutes; //subtracts minutes from user input
+                    if (userIn > 0){
+                        outputBox.Text += System.Convert.ToString(", ");
+                    } else {
+                        outputBox.Text += System.Convert.ToString(" ");
+                    }
+                }
+                if (userIn > 0) {
+                    if(userIn > 1) { //Outputs remaining seconds
+                    outputBox.Text += System.Convert.ToString(userIn + " Seconds");
+                    } else { 
+                    outputBox.Text += System.Convert.ToString(userIn + " Second");
+                    }
+                }
+            }
+                
+
+            
+        }
+    }
+}
