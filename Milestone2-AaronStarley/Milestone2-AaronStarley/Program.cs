427a59ef261d2d3de64cfb1097f9237d29ef74fa
Aaron Starley <AStarley@my.gcu.edu>
2022-12-14 19:33:32 -0700

    12-14 Let's try that again

--- Milestone2-AaronStarley/Milestone2-AaronStarley/Program.cs
+++ Milestone2-AaronStarley/Milestone2-AaronStarley/Program.cs
@@ -3,9 +3,6 @@ Aaron Starley
 12-14-2022
 CST-150-O500
 
-The "new console template used by default is throwing me off because
-there is no main. I'm not going to mess with it.
-
 This program contains my "Item" class.
 A "aluminum can" "Item" is created below. Different getters and setters
 are manipulated.
@@ -20,69 +17,76 @@ worry more.
 
 
 // See https://aka.ms/new-console-template for more information
+/*I don't like that Visual Studio does this ^
+ * I'll have to find how to turn that "feature" off next time. */ 
+
+
+
 using System.Reflection.Metadata.Ecma335;
-
-
-//Creating a Item object
-Item AluminumCan = new Item("Aluminum Can", 12345678, 100000000001, 12, 2.05F);
-Item Default = new Item();
-
-Console.WriteLine("Item");
-Console.WriteLine(AluminumCan.name);
-Console.WriteLine("$"+AluminumCan.price);
-Console.WriteLine(AluminumCan.stock+" in stock");
-AluminumCan.discountPercent=0.15F;
-Console.WriteLine("$"+AluminumCan.discountPrice.ToString("0.00"));
-Console.WriteLine("------------------------------------");
-Console.WriteLine("Item");
-Console.WriteLine(Default.name);
-Console.WriteLine("$"+Default.price);
-Console.WriteLine(Default.stock+" in stock");
-
-class Item { 
-    private string Name;
-    private int SKU;
-    private long UPC;
-    int Stock;
-    float Price;
-    float DiscountPercent = 0;
-    public Item(string name, int sku, long upc, int stock, float price) { 
-        Name = name;
-        SKU = sku;
-        UPC = upc;
-        Stock = stock;
-        Price = price;
-    }    
-    public Item() { 
-        Name = "Turbo Encabulator";
-        SKU = 12311962;
-        UPC = 0000000000000000;
-        Stock = 0;
-        Price = 750000000.00F;
+namespace Milestone2AaronStarley{
+public static class Program { 
+    public static void Main() { 
+    //Creating a Item object
+        Item AluminumCan = new Item("Aluminum Can", 12345678, 100000000001, 12, 2.05F);
+        Item Default = new Item();
+        
+        Console.WriteLine("Item");
+        Console.WriteLine(AluminumCan.name);
+        Console.WriteLine("$"+AluminumCan.price);
+        Console.WriteLine(AluminumCan.stock+" in stock");
+        AluminumCan.discountPercent=0.15F;
+        Console.WriteLine("$"+AluminumCan.discountPrice.ToString("0.00"));
+        Console.WriteLine("------------------------------------");
+        Console.WriteLine("Item");
+        Console.WriteLine(Default.name);
+        Console.WriteLine("$"+Default.price);
+        Console.WriteLine(Default.stock+" in stock");
+        }
     }
-    public string name {
-        get { return Name;}
-    }
-    public int sku { 
-        get { return SKU;}
-    }
-    public long upc {
-        get { return UPC;}    
-    }
-    public int stock {
-        get { return Stock;}
-        set { Stock = value;}
-    }
-    public float price { 
-        get { return Price;}
-        set { Price = value;}
-    }
-    public float discountPercent {
-        get { return DiscountPercent;}
-        set { DiscountPercent = value;}
-    }
-    public float discountPrice {
-        get { return price*(1-discountPercent);}//I think I'm doing my math right...
-    }
-
+    class Item { 
+        private string Name;
+        private int SKU;
+        private long UPC;
+        int Stock;
+        float Price;
+        float DiscountPercent = 0;
+        public Item(string name, int sku, long upc, int stock, float price) { 
+            Name = name;
+            SKU = sku;
+            UPC = upc;
+            Stock = stock;
+            Price = price;
+        }    
+        public Item() { 
+            Name = "Turbo Encabulator";
+            SKU = 12311962;
+            UPC = 0000000000000000;
+            Stock = 0;
+            Price = 750000000.00F;
+        }
+        public string name {
+            get { return Name;}
+        }
+        public int sku { 
+            get { return SKU;}
+        }
+        public long upc {
+            get { return UPC;}    
+        }
+        public int stock {
+            get { return Stock;}
+            set { Stock = value;}
+        }
+        public float price { 
+            get { return Price;}
+            set { Price = value;}
+        }
+        public float discountPercent {
+            get { return DiscountPercent;}
+            set { DiscountPercent = value;}
+        }
+        public float discountPrice {
+            get { return price*(1-discountPercent);}//I think I'm doing my math right...
+        }
+    }  
 }
